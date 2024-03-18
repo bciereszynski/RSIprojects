61b478fe7b6f5961e35fd24bfff80bafc4286c8d
bciereszynski <b.ciereszynski@wp.pl>
2024-03-18 14:31:14 +0100

    SOAP-glassfish add product functionality

--- WebGlass/src/main/java/org/jg/rsi/HelloWorld.java
+++ WebGlass/src/main/java/org/jg/rsi/HelloWorld.java
@@ -4,10 +4,15 @@ import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
 import javax.jws.soap.SOAPBinding.Use;
+import java.util.List;
+
 //Service Endpoint Interface
 @WebService
 @SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL) //optional
 public interface HelloWorld {
     @WebMethod
     String getHelloWorldAsString(String name);
+
+    @WebMethod
+    public List<Product> getProducts();
 }
--- WebGlass/src/main/java/org/jg/rsi/HelloWorldImpl.java
+++ WebGlass/src/main/java/org/jg/rsi/HelloWorldImpl.java
@@ -1,10 +1,22 @@
 package org.jg.rsi;
 //Service Implementation
 import javax.jws.WebService;
+import java.util.ArrayList;
+import java.util.List;
+
 @WebService(endpointInterface = "org.jg.rsi.HelloWorld")
 public class HelloWorldImpl implements HelloWorld {
     @Override
     public String getHelloWorldAsString(String name) {
         return "Witaj świecie JAX-WS: " + name;
+    }
+    @Override
+    public List<Product> getProducts(){
+        Product p1 = new Product("nazwa1", "desc", 12);
+        Product p2 = new Product("nazwa2", "desc2", 21);
+        List<Product> list = new ArrayList<>();
+        list.add(p1);
+        list.add(p2);
+        return list;
     }
 }
